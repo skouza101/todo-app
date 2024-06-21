858158e0b9ee46acdd77ffb3c510d37033c6ee70
oraichezakariyae <164167114+oraichezakariyae@users.noreply.github.com>
2024-06-21 18:24:48 +0100

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,36 @@
+'use client'
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import Task from "@/components/Task";
+import { useState } from "react";
+import {v4 as uuidv4} from "uuid"
+uuidv4()
+
+export default function Home() {
+  const [todos , setTodo] = useState([])
+
+  const createTodo = (todo) => {
+    setTodo([...todos , {id : uuidv4(), task:todo , isEnding : false}])
+    console.log(todos)
+  }
+
+  const deleteTodo = id => {
+    setTodo(todos.filter(todo => todo.id !== id))
+  }
+
+  const updateTodo = (newValue , id)=> {
+    setTodo(todos.map(todo => todo.id === id ? {...todos , task : newValue ,isEnding : true} : console.log("canceled")))
+  }
+
+  const updateTask = id=> {
+    setTodo(todos.map(todo => todo.id === id ? {...todos  ,isEnding : true} : console.log("canceled")))
+  }
+
+  return (
+    <main className="w-screen min-h-full px-32 relative flex flex-col">
+      <Header />
+      <Task data = {todos} deleteTodo = {deleteTodo} updateTodo={updateTodo} updateTask={updateTask}/>
+      <Form createTodo = { createTodo }/>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("daisyui"), function ({ addUtilities }) {
+      const newUtilities = {
+        /* Hide scrollbar for Chrome, Safari, and Opera */
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        /* Hide scrollbar for IE, Edge, and Firefox */
+        // Add other browser-specific styles here if needed
+      };
+      addUtilities(newUtilities);
+    },],
+  daisyui: {
+    themes: ["valentine", "dark", "night","aqua",{
+      mytheme: {
+         
+ "primary": "#38bdf8",
+          
+ "secondary": "#38bdf8",
+          
+ "accent": "#38bdf8",
+          
+ "neutral": "#38bdf8",
+          
+ "base-100": "#e0f2fe",
+          
+ "info": "#0ea5e9",
+          
+ "success": "#557800",
+          
+ "warning": "#dd0000",
+          
+ "error": "#ff3e68",
+      },
+    },
+      ],
+  },
+};
